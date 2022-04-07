fddcfd7e939226140e92d9c66cef1035e6f6ac94
Sanjay <sanjay8874002308@gmail.com>
2022-04-07 22:09:01 +0530

    added list

--- Python_02092021.py
+++ Python_02092021.py
@@ -105,10 +105,24 @@ def Mult(a,b):
 add(5, 5)
 deff(5,5)
 division(10,5)
-Mult(5,5)        
-        
+Mult(5,5)
 
-######
+
+#greater no
+
+
+def max(a,b):
+    if a>b:
+        print("a is grater then b")
+    else:
+        print("b is grater then a")
+        
+max(10,11)        
+
+#list
+
+List = [1,2,4,5,6,7,8,9,45,"sanjay"]
+print(List)
 
 
 
